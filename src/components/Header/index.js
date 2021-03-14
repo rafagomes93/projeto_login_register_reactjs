3ac90e9fc5a87a127520ab3f26aabb1f78835bf9
Rafael Gomes <73504379+rafagomes93@users.noreply.github.com>
2021-03-13 21:07:45 -0300

    Update index.js
    Corrigindo a palavra 'header'.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.css';
 
-export default function Headers(){
+export default function Header(){
     return(
         <header id='main-header'>
             <div className='header-content'>
